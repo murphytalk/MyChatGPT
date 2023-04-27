d41b8961b166365575befe65686847eaa2615abc
Mu Lu <murphytalk@gmail.com>
2023-04-27 22:20:21 +0900

    disable input widget while waiting for answer

--- lib/open_ai.dart
+++ lib/open_ai.dart
@@ -9,38 +9,42 @@ class OpenAIChat extends StatefulWidget {
   const OpenAIChat({super.key});
 
   @override
-  OpenAIChatState createState() => OpenAIChatState();
+  _OpenAIChatState createState() => _OpenAIChatState();
 }
 
-
-class OpenAIChatState extends State<OpenAIChat> with RouteAware{
+class _OpenAIChatState extends State<OpenAIChat> with RouteAware {
   final TextEditingController _textController = TextEditingController();
   List<Message> _messages = [];
   String? _curConversationId;
   bool _thinking = false;
   bool _loadConversation = false;
   bool _observerRegistered = false;
+  bool _canAskQuestion = true;
 
-  static const _prompt = 'AI tutor,answer my question concisely without elaboration';
+  static const _prompt =
+      'AI tutor,answer my question concisely without elaboration';
 
-  Future<String> _getOpenAiResponse(List<Message> messages) async{
+  Future<String> _getOpenAiResponse(List<Message> messages) async {
     final first = OpenAIChatCompletionChoiceMessageModel(
       content: _prompt,
       role: OpenAIChatMessageRole.user,
     );
-    final completions = await OpenAI.instance.chat.create(
-        model: "gpt-3.5-turbo",
-        messages: [first, ... messages.map( (m) =>
-          OpenAIChatCompletionChoiceMessageModel(
-            content: m.content,
-          role: OpenAIChatMessageRole.user,
-        )).toList(growable: false)]
-    );
+    final completions =
+        await OpenAI.instance.chat.create(model: "gpt-3.5-turbo", messages: [
+      first,
+      ...messages
+          .map((m) => OpenAIChatCompletionChoiceMessageModel(
+                content: m.content,
+                role: OpenAIChatMessageRole.user,
+              ))
+          .toList(growable: false)
+    ]);
     storage.answer(completions);
     return completions.choices[0].message.content;
   }
 
-  void _sendMessage(BuildContext ctx,String message) async {
+  Future<void> _sendMessage(BuildContext ctx, String message) async {
+    log('sending msg $message');
     setState(() {
       _messages.add(Message(fromAI: false, content: message));
     });
@@ -48,7 +52,7 @@ class OpenAIChatState extends State<OpenAIChat> with RouteAware{
     storage.question(message);
 
     setState(() {
-       _thinking = true;
+      _thinking = true;
     });
 
     try {
@@ -57,8 +61,7 @@ class OpenAIChatState extends State<OpenAIChat> with RouteAware{
         _thinking = false;
         _messages.add(Message(fromAI: true, content: response));
       });
-    }
-    catch(e){
+    } catch (e) {
       showErrorDialog(ctx, e.toString());
     }
   }
@@ -66,7 +69,7 @@ class OpenAIChatState extends State<OpenAIChat> with RouteAware{
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
-    if(!_observerRegistered) {
+    if (!_observerRegistered) {
       log('route observer registered');
       _observerRegistered = true;
       routeObserver.subscribe(this, ModalRoute.of(context)!);
@@ -82,7 +85,7 @@ class OpenAIChatState extends State<OpenAIChat> with RouteAware{
 
   @override
   void didPopNext() {
-    if(AppState().conversationToLoad.isNotEmpty){
+    if (AppState().conversationToLoad.isNotEmpty) {
       setState(() => _loadConversation = true);
     }
   }
@@ -98,12 +101,12 @@ class OpenAIChatState extends State<OpenAIChat> with RouteAware{
               final content = _messages[index].content;
               return isMarkdown
                   ? Markdown(
-                data: content,
-                shrinkWrap: true,
-              )
+                      data: content,
+                      shrinkWrap: true,
+                    )
                   : ListTile(
-                title: Text(content),
-              );
+                      title: Text(content),
+                    );
             },
           ),
         ),
@@ -115,18 +118,18 @@ class OpenAIChatState extends State<OpenAIChat> with RouteAware{
                 icon: const Icon(Icons.add_circle),
                 onPressed: () {
                   _textController.clear();
-                  storage.newConversation([], "mu", _textController.text)
-                      .then((v) {
+                  storage.newConversation([], "mu", _textController.text).then(
+                      (v) {
                     _curConversationId = v;
                     _textController.clear();
-                  })
-                      .catchError((e) {
+                  }).catchError((e) {
                     showErrorDialog(context, e.toString());
                   });
                 },
               ),
               Expanded(
                 child: TextField(
+                  enabled: _canAskQuestion,
                   controller: _textController,
                   decoration: const InputDecoration(
                     labelText: 'Ask a question',
@@ -139,23 +142,22 @@ class OpenAIChatState extends State<OpenAIChat> with RouteAware{
                   var q = _textController.text.trim();
                   if (q.isNotEmpty) {
                     if (_curConversationId == null) {
-                      storage.newConversation(
-                          [], AppState().user.name, q)
-                          .then((value) {
+                      setState(() => _canAskQuestion = false);
+                      storage.newConversation([], AppState().user.name, q).then(
+                          (value) {
                         _curConversationId = value;
                         _textController.clear();
-                        _sendMessage(context, q);
-                      })
-                          .catchError((e) {
+                        _sendMessage(context, q).whenComplete(
+                            () => setState(() => _canAskQuestion = true));
+                      }).catchError((e) {
                         showErrorDialog(context, e.toString());
                       });
-                    }
-                    else {
+                    } else {
                       storage.question(q).then((_) {
-                        _sendMessage(context, q);
+                        _sendMessage(context, q).whenComplete(
+                            () => setState(() => _canAskQuestion = true));
                         _textController.clear();
-                      })
-                          .catchError((e) {
+                      }).catchError((e) {
                         showErrorDialog(context, e.toString());
                       });
                     }
@@ -166,50 +168,53 @@ class OpenAIChatState extends State<OpenAIChat> with RouteAware{
           ),
         ),
       ],
-    );}
+    );
+  }
 
-    @override
-    Widget build(BuildContext context) {
-      if(_thinking) return const Center(child: AwaitWidget(caption: "Thinking ..."));
-
-      if(_loadConversation && AppState().conversationToLoad.isNotEmpty){
-        _curConversationId = AppState().conversationToLoad.uuid;
-        return FutureBuilder(
-            future: storage.getConversation(AppState().conversationToLoad.uuid),
-            builder: (ctx, conversation){
-              AppState().conversationToLoad = ConversationInfo.empty();
-              _loadConversation = false;
-              if(conversation.hasData){
-                storage.resumeConversation(_curConversationId!);
-                _messages = conversation.data?.messages ?? [];
-                return _buildNormalUi(context);
-              }
-              else if(conversation.hasError){
-                _curConversationId = null;
-                if(conversation.error !=null){
-                  return AlertDialog(
-                    title: const Text('Could not load conversion'),
-                    content: Text(conversation.error.toString()),
-                    actions: <Widget>[
-                      TextButton(
-                        child: const Text('Close'),
-                        onPressed: () {
-                          //trigger rebuild
-                          setState((){});
-                        },
-                      ),
-                    ],
-                  );
-                }
-                return _buildNormalUi(context);
-              }
-              else {
-                return Center(child: AwaitWidget(caption: "Loading ${AppState().conversationToLoad.topic}",));
-              }
-            });
-      }
-      else {
-        return _buildNormalUi(context);
-      }
+  @override
+  Widget build(BuildContext context) {
+    if (_thinking) {
+      return const Center(child: AwaitWidget(caption: "Thinking ..."));
     }
+
+    if (_loadConversation && AppState().conversationToLoad.isNotEmpty) {
+      _curConversationId = AppState().conversationToLoad.uuid;
+      return FutureBuilder(
+          future: storage.getConversation(AppState().conversationToLoad.uuid),
+          builder: (ctx, conversation) {
+            AppState().conversationToLoad = ConversationInfo.empty();
+            _loadConversation = false;
+            if (conversation.hasData) {
+              storage.resumeConversation(_curConversationId!);
+              _messages = conversation.data?.messages ?? [];
+              return _buildNormalUi(context);
+            } else if (conversation.hasError) {
+              _curConversationId = null;
+              if (conversation.error != null) {
+                return AlertDialog(
+                  title: const Text('Could not load conversation'),
+                  content: Text(conversation.error.toString()),
+                  actions: <Widget>[
+                    TextButton(
+                      child: const Text('Close'),
+                      onPressed: () {
+                        //trigger rebuild
+                        setState(() {});
+                      },
+                    ),
+                  ],
+                );
+              }
+              return _buildNormalUi(context);
+            } else {
+              return Center(
+                  child: AwaitWidget(
+                caption: "Loading ${AppState().conversationToLoad.topic}",
+              ));
+            }
+          });
+    } else {
+      return _buildNormalUi(context);
+    }
+  }
 }
